776b64a2d40d7916d907ca8c9e35709935ccef93
Ngọc Minh Lê <minh220055@gmail.com>
2026-08-19 00:06:40 +0700

    Disable client caching by setting Cache-Control headers in app.py

--- app.py
+++ app.py
@@ -13,6 +13,13 @@ import re
 app = Flask(__name__)
 faker = Faker('vi_VN')
 
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
+
 def clean_google_form_url(url):
     if not url:
         return url
